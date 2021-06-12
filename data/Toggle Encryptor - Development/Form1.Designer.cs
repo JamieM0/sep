6ee2b418d1f8a20d4b33e777dffdb5554d8a49da
Jamie Matthews <41803205+Jamium@users.noreply.github.com>
2021-06-12 19:25:57 +0100

    * Added Instructions

--- data/Toggle Encryptor - Development/Form1.Designer.cs
+++ data/Toggle Encryptor - Development/Form1.Designer.cs
@@ -32,8 +32,23 @@ namespace Toggle_Encryptor___Development
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbHeading = new System.Windows.Forms.Label();
             this.gbAbout = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnQuitFromAbout = new System.Windows.Forms.Button();
+            this.btnContinueFromAbout = new System.Windows.Forms.Button();
             this.lbAbout1 = new System.Windows.Forms.Label();
+            this.gbMain = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConfirmInputEncrypt = new System.Windows.Forms.Button();
+            this.txtEncryptInput = new System.Windows.Forms.TextBox();
+            this.lbIns2 = new System.Windows.Forms.Label();
+            this.btnConfirmPass = new System.Windows.Forms.Button();
+            this.btnGenPass = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lbIns1 = new System.Windows.Forms.Label();
             this.gbAbout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbHeading
@@ -48,12 +63,47 @@ namespace Toggle_Encryptor___Development
             // 
             // gbAbout
             // 
+            this.gbAbout.Controls.Add(this.pictureBox1);
+            this.gbAbout.Controls.Add(this.btnQuitFromAbout);
+            this.gbAbout.Controls.Add(this.btnContinueFromAbout);
             this.gbAbout.Controls.Add(this.lbAbout1);
             this.gbAbout.Location = new System.Drawing.Point(12, 66);
             this.gbAbout.Name = "gbAbout";
             this.gbAbout.Size = new System.Drawing.Size(734, 319);
             this.gbAbout.TabIndex = 1;
             this.gbAbout.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnQuitFromAbout
+            // 
+            this.btnQuitFromAbout.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnQuitFromAbout.Location = new System.Drawing.Point(28, 253);
+            this.btnQuitFromAbout.Name = "btnQuitFromAbout";
+            this.btnQuitFromAbout.Size = new System.Drawing.Size(90, 47);
+            this.btnQuitFromAbout.TabIndex = 2;
+            this.btnQuitFromAbout.Text = "Quit TEPP";
+            this.btnQuitFromAbout.UseVisualStyleBackColor = true;
+            // 
+            // btnContinueFromAbout
+            // 
+            this.btnContinueFromAbout.BackColor = System.Drawing.Color.Yellow;
+            this.btnContinueFromAbout.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnContinueFromAbout.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnContinueFromAbout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnContinueFromAbout.Location = new System.Drawing.Point(565, 253);
+            this.btnContinueFromAbout.Name = "btnContinueFromAbout";
+            this.btnContinueFromAbout.Size = new System.Drawing.Size(147, 47);
+            this.btnContinueFromAbout.TabIndex = 1;
+            this.btnContinueFromAbout.Text = "Continue ->";
+            this.btnContinueFromAbout.UseVisualStyleBackColor = false;
+            this.btnContinueFromAbout.Click += new System.EventHandler(this.btnContinueFromAbout_Click);
             // 
             // lbAbout1
             // 
@@ -64,17 +114,125 @@ namespace Toggle_Encryptor___Development
             this.lbAbout1.TabIndex = 0;
             this.lbAbout1.Text = resources.GetString("lbAbout1.Text");
             // 
+            // gbMain
+            // 
+            this.gbMain.Controls.Add(this.label2);
+            this.gbMain.Controls.Add(this.label1);
+            this.gbMain.Controls.Add(this.btnConfirmInputEncrypt);
+            this.gbMain.Controls.Add(this.txtEncryptInput);
+            this.gbMain.Controls.Add(this.lbIns2);
+            this.gbMain.Controls.Add(this.btnConfirmPass);
+            this.gbMain.Controls.Add(this.btnGenPass);
+            this.gbMain.Controls.Add(this.txtPass);
+            this.gbMain.Controls.Add(this.lbIns1);
+            this.gbMain.Location = new System.Drawing.Point(12, 66);
+            this.gbMain.Name = "gbMain";
+            this.gbMain.Size = new System.Drawing.Size(734, 319);
+            this.gbMain.TabIndex = 3;
+            this.gbMain.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(52, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "abcimherehehe";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(38, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "3. Here\'s the encrypted phrase.";
+            // 
+            // btnConfirmInputEncrypt
+            // 
+            this.btnConfirmInputEncrypt.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnConfirmInputEncrypt.ForeColor = System.Drawing.Color.Black;
+            this.btnConfirmInputEncrypt.Location = new System.Drawing.Point(517, 160);
+            this.btnConfirmInputEncrypt.Name = "btnConfirmInputEncrypt";
+            this.btnConfirmInputEncrypt.Size = new System.Drawing.Size(101, 32);
+            this.btnConfirmInputEncrypt.TabIndex = 6;
+            this.btnConfirmInputEncrypt.Text = "Confirm";
+            this.btnConfirmInputEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // txtEncryptInput
+            // 
+            this.txtEncryptInput.Location = new System.Drawing.Point(52, 169);
+            this.txtEncryptInput.Name = "txtEncryptInput";
+            this.txtEncryptInput.Size = new System.Drawing.Size(366, 23);
+            this.txtEncryptInput.TabIndex = 5;
+            // 
+            // lbIns2
+            // 
+            this.lbIns2.AutoSize = true;
+            this.lbIns2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbIns2.Location = new System.Drawing.Point(38, 128);
+            this.lbIns2.Name = "lbIns2";
+            this.lbIns2.Size = new System.Drawing.Size(406, 25);
+            this.lbIns2.TabIndex = 4;
+            this.lbIns2.Text = "2. Enter a string to encrypt with your password.";
+            // 
+            // btnConfirmPass
+            // 
+            this.btnConfirmPass.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnConfirmPass.ForeColor = System.Drawing.Color.Black;
+            this.btnConfirmPass.Location = new System.Drawing.Point(517, 55);
+            this.btnConfirmPass.Name = "btnConfirmPass";
+            this.btnConfirmPass.Size = new System.Drawing.Size(101, 32);
+            this.btnConfirmPass.TabIndex = 3;
+            this.btnConfirmPass.Text = "Confirm";
+            this.btnConfirmPass.UseVisualStyleBackColor = true;
+            // 
+            // btnGenPass
+            // 
+            this.btnGenPass.Location = new System.Drawing.Point(291, 63);
+            this.btnGenPass.Name = "btnGenPass";
+            this.btnGenPass.Size = new System.Drawing.Size(127, 23);
+            this.btnGenPass.TabIndex = 2;
+            this.btnGenPass.Text = "Generate a Password";
+            this.btnGenPass.UseVisualStyleBackColor = true;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(52, 64);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(233, 23);
+            this.txtPass.TabIndex = 1;
+            // 
+            // lbIns1
+            // 
+            this.lbIns1.AutoSize = true;
+            this.lbIns1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbIns1.Location = new System.Drawing.Point(38, 24);
+            this.lbIns1.Name = "lbIns1";
+            this.lbIns1.Size = new System.Drawing.Size(395, 25);
+            this.lbIns1.TabIndex = 0;
+            this.lbIns1.Text = "1. Enter a password to encrypt the string with.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.gbAbout);
             this.Controls.Add(this.lbHeading);
+            this.Controls.Add(this.gbMain);
+            this.Controls.Add(this.gbAbout);
             this.Name = "Form1";
             this.Text = "Toggle Encryptor - Prototype";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.gbAbout.ResumeLayout(false);
             this.gbAbout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbMain.ResumeLayout(false);
+            this.gbMain.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +243,19 @@ namespace Toggle_Encryptor___Development
         private System.Windows.Forms.Label lbHeading;
         private System.Windows.Forms.GroupBox gbAbout;
         private System.Windows.Forms.Label lbAbout1;
+        private System.Windows.Forms.Button btnQuitFromAbout;
+        private System.Windows.Forms.Button btnContinueFromAbout;
+        private System.Windows.Forms.GroupBox gbMain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtEncryptInput;
+        private System.Windows.Forms.Label lbIns2;
+        private System.Windows.Forms.Button btnConfirmPass;
+        private System.Windows.Forms.Button btnGenPass;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lbIns1;
+        private System.Windows.Forms.Button btnConfirmInputEncrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- data/Toggle Encryptor - Development/Form1.cs
+++ data/Toggle Encryptor - Development/Form1.cs
@@ -15,7 +15,43 @@ namespace Toggle_Encryptor___Development
         public Form1()
         {
             InitializeComponent();
+            gbAbout__enable();
+            gbMain__disable();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gbAbout__disable()
+        {
+            gbAbout.Visible = false;
+            gbAbout.Enabled = false;
+        }
+
+        private void gbAbout__enable()
+        {
+            gbAbout.Visible = true;
+            gbAbout.Enabled = true;
+        }
+
+        private void gbMain__disable()
+        {
+            gbMain.Visible = false;
+            gbMain.Enabled = false;
+        }
+
+        private void gbMain__enable()
+        {
+            gbMain.Visible = true;
+            gbMain.Enabled = true;
+        }
+
+        private void btnContinueFromAbout_Click(object sender, EventArgs e)
+        {
+            gbAbout__disable();
+            gbMain__enable();
+        }
     }
 }
